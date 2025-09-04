1c4f36b260384bd027d7dca0ca76fcc15e6324b7
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-09-04 13:52:16 +0200

    Adde the image import so the error stoped and added a SafeAreaView so that the image is not layered over the text.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import React from 'react';
 import {  Button } from 'react-native';
 
@@ -18,12 +18,18 @@ export default function App() {
     }
 
   return (
+    <SafeAreaView>
     <View style={styles.container}>
+      <Image source={{uri:'https://cdn.pixabay.com/photo/2017/10/03/15/54/car-2812943_1280.jpg',
+      
+      }}
+      style={styles.Image}/> 
       <Text style ={styles.response}>
          {feedback}
       </Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
@@ -41,5 +47,10 @@ const styles = StyleSheet.create({
     fontSize: 28,
      color: 'af3'
   
+  },
+  Image:{
+    width: 400,
+  height: 350
   }
+  
 });
