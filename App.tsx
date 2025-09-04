2b0a0194a6fc37db5cbeaa6af1604e181e51d168
Not4PplPrsn <ST10470237@vcconnect.edu.za>
2025-09-04 13:18:20 +0200

    Added new responses when the user presses tje buttons am d and changed the color of the background and the text.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {  Button } from 'react-native';
+
 
 export default function App() {
+  const age = 16;
+  let feedback = '';
+  const yearsexp = 0;
+
+  if ( age >= 18 && yearsexp >= 3)
+    {
+      feedback = "You are allowed to drive."
+    } 
+    else {
+      feedback = "Still too young."
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style ={styles.response}>
+         {feedback}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#afe',
     alignItems: 'center',
     justifyContent: 'center',
+   
+    
   },
+
+  response:{
+    fontSize: 28,
+     color: 'af3'
+  
+  }
 });
